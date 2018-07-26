07a900f46e9dab931fd4f471aad4da3f452b0370
Evgenii Nikitin <mail.nikitin@mail.ru>
2018-07-26 22:12:35 +0300

    fix date filter for TransactionsByFilter

--- src/Requests/Transactions/TransactionsByFilter.php
+++ src/Requests/Transactions/TransactionsByFilter.php
@@ -24,12 +24,12 @@ class TransactionsByFilter extends Request
 
 
     public function setStartDate(Carbon $startDate){
-        $this->params['start_date'] = $startDate;
+        $this->params['start_date'] = $startDate->toDateString();
         return $this;
     }
 
     public function setEndDate(Carbon $endDate){
-        $this->params['end_date'] = $endDate;
+        $this->params['end_date'] = $endDate->toDateString();
         return $this;
     }
 
